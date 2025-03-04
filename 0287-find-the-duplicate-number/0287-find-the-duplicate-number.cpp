96da5fb5e5d7b04a88c709507f658012d4c0fab2
Rajnish Kumar Singh <kumar.rajnish6587@gmail.com>
2025-03-04 11:57:56 +0530

    Time: 0 ms (100%), Space: 65.3 MB (25.27%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n;i++){
             vector<bool> matvisit(n,false);
             for(int j=0;j<n;j++){
                 int visit=nums[j]-1;
@@ -11,7 +10,6 @@ public:
                 }
                 matvisit[visit]=true;
             }
-        }
         return 0;
     }
 };
