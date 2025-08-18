44ce44a2e34aaf5a789498c595c486406f57f29b
Rajnish Kumar Singh <kumar.rajnish6587@gmail.com>
2025-08-18 13:51:15 +0530

    Time: 102 ms (10.79%), Space: 38.5 MB (8.59%) - LeetHub

---
+++ 0679-24-game/0679-24-game.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> nums;
+        for(int c:cards) nums.push_back((double)c);
+        return solve(nums);
+    }
+    private:
+    bool solve(vector<double>&nums){
+        if(nums.size()==1){
+            return fabs(nums[0]-24.0)<1e-6;
+        }
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(i==j) continue;
+
+                vector<double>next;
+                for(int k=0;k<nums.size();k++){
+                    if(k!=i&&k!=j) next.push_back(nums[k]);
+                }
+                for(double val:compute(nums[i],nums[j])){
+                    next.push_back(val);
+                    if(solve(next)) return true;
+                    next.pop_back();
+                }
+            }
+        } 
+        return false;
+    }
+
+    vector<double> compute(double a,double b){
+        vector<double> res;
+        res.push_back(a+b);
+        res.push_back(a-b);
+        res.push_back(b-a);
+        res.push_back(a*b);
+        if(fabs(b)>1e-6) res.push_back(a/b);
+        if(fabs(a)>1e-6) res.push_back(b/a);
+        return res;
+
+    }
+};
